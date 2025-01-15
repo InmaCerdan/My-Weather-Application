9cb3a61088e53f32bc2924283c01311734fd04f3
InmaCerdan <inmacerdan96@gmail.com>
2025-01-15 13:05:08 +0000

    Injected weather date and current time

--- src/index.js
+++ src/index.js
@@ -2,8 +2,45 @@ function updateInfo(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-app-city");
+  let conditionElement = document.querySelector("#condition");
+  let condition = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let year = document.querySelector("#year");
+
   cityElement.innerHTML = response.data.city;
+
+  timeElement.innerHTML = formatDate(date);
+  year.innerHTML = date.getFullYear();
+  conditionElement.innerHTML =
+    condition.charAt(0).toUpperCase() + condition.slice(1);
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
